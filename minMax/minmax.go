35979172aec7ba8d05acd45b45aa52f3e882bb02
PuggyPotato <qingyaurou@gmail.com>
2025-04-18 22:19:48 +0800

    Made It more idiomatic,minmax

--- minMax/minmax.go
+++ minMax/minmax.go
@@ -13,20 +13,20 @@ func main(){
 		fmt.Print("Enter Number:")
 		fmt.Scan(&currentNum)
 		fmt.Println("To Quit Enter -99")
-		if(currentNum == -99){
+		if currentNum == -99{
 			break;
 		}
 		arrNum = append(arrNum, currentNum)
 	}
-	if(len(arrNum) !=0){
+	if len(arrNum) !=0{
 		var max =arrNum[0]
 		var min =arrNum[0]
-		for i:= 0 ; i < len(arrNum); i++{
-			if(arrNum[i] > max){
-				max = arrNum[i]
+		for _,num:=range arrNum{
+			if num > max{
+				max = num
 			}
-			if(arrNum[i] < min){
-				min = arrNum[i]
+			if num < min{
+				min = num
 			}
 		}
 		fmt.Println("The Max Is:",max)
